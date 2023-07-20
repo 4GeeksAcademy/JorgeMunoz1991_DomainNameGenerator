2f87a902c3ded7345cd79eed1d38a10ab9574fd2
Jorge Muñoz Guerrero <131190211+JorgeMunoz1991@users.noreply.github.com>
2023-07-20 17:57:47 +0000

    my project

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   var pronoun = ["the", "our"];
   var adj = ["great", "big"];
   var noun = ["jogger", "racoon"];
   var extensions = [".com", ".net", ".us", ".io", ".es"];
 
-  // Variable para almacenar todas las combinaciones de nombres de dominio
   var domainNames = [];
 
-  // Bucle anidado para generar todas las combinaciones posibles
   for (var i = 0; i < pronoun.length; i++) {
     for (var j = 0; j < adj.length; j++) {
       for (var k = 0; k < noun.length; k++) {
@@ -22,7 +13,6 @@ window.onload = function() {
           var domainName = pronoun[i] + adj[j] + noun[k] + extensions[l];
           domainNames.push(domainName);
         }
-        // Generar "domain hacks" con la extensión formando parte del dominio
         var domainHack =
           pronoun[i] + adj[j] + noun[k].slice(0, -1) + extensions[l];
         domainNames.push(domainHack);
@@ -30,8 +20,10 @@ window.onload = function() {
     }
   }
 
-  // Imprimir las combinaciones de nombres de dominio generadas en la consola
-  for (var x = 0; x < domainNames.length; x++) {
-    console.log(domainNames[x]);
-  }
+  var domainListElement = document.getElementById("domainList");
+  domainNames.forEach(function(domain) {
+    var li = document.createElement("li");
+    li.textContent = domain;
+    domainListElement.appendChild(li);
+  });
 };
